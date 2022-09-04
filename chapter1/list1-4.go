a24c8fb6b4f098d2085a3eb6589372b0e9784315
satoshi-ueshima <ueshima.satoshi@mizai.tech>
2022-09-04 13:56:11 +0900

    2-1完了

---
+++ chapter1/list1-4.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Print("123 * 45 = ")
+	fmt.Println(123*45)
+}
---
+++ chapter2/list2-2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	x := input("type a price")
+	n, err := strconv.Atoi(x)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	p := float64(n)
+	fmt.Println(int(p*1.1))
+}
+
+
+func input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(msg  + ": ")
+	scanner.Scan()
+    return scanner.Text()
+}
